51255ef4607efd8dcc7d0ea03a50764ec68d85c2
adilson554 <adilson554@gmail.com>
2022-08-28 00:48:57 +0000

    primeiro commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class Usuario:
+    def __init__(self, id, nome, telefone):
+        self.id = id
+        self.nome = nome
+        self.telefone = telefone
+
+
+meu = Usuario(123, 'Meu Nome Completo', 99999999999)
+
